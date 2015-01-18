76af49c0c78ec96737431b94535bf052f38d8c4c
Guillaume Raimondeau <raimondeau@gmail.com>
2015-01-18 15:55:33 +0100

    Insufficiently motivated??

--- pset1/bobcount.py
+++ pset1/bobcount.py
@@ -5,10 +5,10 @@ count=len(s)
 num=0
 bobcount=0
 
-for x in s[count]:
+for x in s:
     if s[num]=='b' and s[num+1]=='o' and s[num+2]=='b':
         bobcount += 1
-        count +=1
+        num +=3
     else:
-        count +=1
+        num +=1
 print("Number of bob:" + str(bobcount))
