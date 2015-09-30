fe2aadbcd0e45e59742851ae24079b1e88cf49fb
Guillaume Raimondeau <raimondeau@gmail.com>
2015-09-30 15:28:54 +0200

    THINK!!

--- pset1/bobcount.py
+++ pset1/bobcount.py
@@ -6,10 +6,10 @@ count=0
 
 for y in s[len(s)-1]:
     if y=='b':
-        continue
-    if y=='o':
-        continue 
-    if y=='b':
-        bobcount += 1
+        pass
+        if y=='o':
+            pass
+            if y=='b':
+                bobcount += 1
 
 print("Number of bob:" + str(bobcount))
