72a8d57e8965bb1b88c8a2a1b210a59b07a19472
Guillaume Raimondeau <raimondeau@gmail.com>
2015-09-30 15:24:11 +0200

    Need to think about algorithm...

--- pset1/bobcount.py
+++ pset1/bobcount.py
@@ -5,7 +5,11 @@ bobcount=0
 count=0
 
 for y in s[len(s)-1]:
-    if y==b and y+1==o and y+2==b:
+    if y=='b':
+        continue
+    if y=='o':
+        continue 
+    if y=='b':
         bobcount += 1
 
 print("Number of bob:" + str(bobcount))
