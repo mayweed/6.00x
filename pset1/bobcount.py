ec7ed87fcc1ab9f7c3c18dd91be2e0a4bbebc66f
Guillaume R <klingon357@gmail.com>
2015-11-02 14:10:33 +0100

    Better that way

--- pset1/bobcount.py
+++ pset1/bobcount.py
@@ -4,11 +4,12 @@ s= input('--> ')
 bobcount=0
 count=0
 
-for y in s[len(s)-1]:
-      if s[count]=='b':
-         if s[count+1] =='o':
-            if s[count+2]=='b':
-               bobcount += 1
-      count +=3
+while count < len(s):
+    if s[count:2] == 'bob': 
+        bobcount += 1
+        count +=2
+    else:
+        continue
+        count +=1
 
 print("Number of bob:" + str(bobcount))
