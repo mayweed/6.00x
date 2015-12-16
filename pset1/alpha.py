0976396e1d2e7aec3bf6dd63ce98249f515af116
Guillaume Raimondeau <raimondeau@gmail.com>
2015-12-16 09:51:45 +0100

    new file:   alpha.py

---
+++ pset1/alpha.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+s= input()
+"""
+ALGO
+Translate string s in a list of ord(s) int
+Check if s > s+1
+If not, append the preceding chunk to the list
+Retranslate it to a string?
+"""
+#a new list where append the new chunks
+chunks=[]
+
+
