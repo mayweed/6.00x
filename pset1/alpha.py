8c086069e2bb5b83926ba1fef7a6143bd543ea31
Guillaume Raimondeau <raimondeau@gmail.com>
2015-12-16 15:44:59 +0100

    some links to read on string methods

--- pset1/alpha.py
+++ pset1/alpha.py
@@ -13,16 +13,19 @@ Retranslate it to a string?
 #a new list where append the new chunks
 chunks=[]
 
+#String manip:
+#http://www.pythonforbeginners.com/basics/string-manipulation-in-python
+#http://pythoncentral.io/cutting-and-slicing-strings-in-python/
 #end is i
-start=0
-
 for i in range(len(s)-1):
+    start=0
     if s[i] < s[i+1]:
         continue
     if s[i] == s[i+1]:
         continue
-    if s[i] > s[i+1]:
+    #need to handle end of line...
+    if s[i] > s[i+1] or i+1 == len(s)-1:
         print(s[start:i+1])
-        print(start,i,file=sys.stderr)
+        print(start, i+1, file=sys.stderr)
         #la fin devt le nouveau début
         start = i+1
