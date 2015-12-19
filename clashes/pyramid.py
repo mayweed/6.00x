36055c693d8c43f24e46b3bfdae0240d68728216
Guillaume Raimondeau <raimondeau@gmail.com>
2015-12-19 18:46:17 +0100

    another nice clash I need more than 5mn to solve it!!(10/15mn)

---
+++ clashes/pyramid.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+"""
+Given a certain number you should print a pyramid of that number:
+Input: 3
+Output:
+333
+33
+3
+INTERESTING: learning print() formatting
+"""
+n=int(input())
+i=0
+z=n
+
+while i <= n:
+    for y in range(z):
+    # no newline, new end
+        print(n,end="")
+    z -=1
+    i += 1
+    print(end="\n")
