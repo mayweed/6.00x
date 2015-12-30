d1ea8c460c96546803bab2267cca4d3edb4885f8
Guillaume Raimondeau <raimondeau@gmail.com>
2015-12-30 10:00:45 +0100

    put data in a list

--- tools/test_stats_csv.py
+++ tools/test_stats_csv.py
@@ -2,11 +2,15 @@
 
 import sys
 import csv
+import pandas as pd
 
 #Dont want to lose time typing and retyping.
 # Should use arg instead of file name in open()'STATS_PRETS_2008_2015.csv'
-# Should add csv.NONNUMERIC: cant convert string to float..
+# Should add csv.QUOTE_NONNUMERIC: cant convert string to float..,quoting=csv.QUOTE_NONNUMERIC)
+data = []
+
 with open(sys.argv[1], newline='') as file:
-    filereader=csv.reader(file,delimiter=',',quotechar='"',quoting=csv.QUOTE_NONNUMERIC)
+    filereader=csv.reader(file,delimiter=',',quotechar='"')
     for row in filereader:
-        print(row)
+        data.append(row)
+print(data[0])
