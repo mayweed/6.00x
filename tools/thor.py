a1e7d614a2f1ea08b42fcc79d771b117259c5348
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-10 20:12:52 +0100

    modified:   tools/thor.py

--- tools/thor.py
+++ tools/thor.py
@@ -6,3 +6,9 @@ while 1:
     if t<y:Y='S';t+=1   
     if T<x:X='E'
     print(Y+X)
+
+"""
+After some tweaks:
+x,y,T,t=[int(i)for i in input().split()]
+while 1:X='W'if T>x else'E'if T<x else'';Y='S'if t<y else'';t+=1;print(Y+X)
+"""
