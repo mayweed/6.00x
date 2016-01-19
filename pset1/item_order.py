6336740c6ca2074b84414d2e42989f149bd1c6c3
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-19 09:01:09 +0100

    Mmm

--- pset1/item_order.py
+++ pset1/item_order.py
@@ -12,16 +12,22 @@ items=list(s.split())
 count_items={}
 
 count=0
-prev=items[0]
+y=0
 
-for i in items:
-    print(i)
-    if i==prev:
-        count +=1
-        count_items[i]=count
-    if i != prev or items.index(i) == len(items)-1:
-        count_items[prev]=count
-        prev=i
-        count=1
+#pour chaque prev il faut parcourir toute la liste
+while y < len(items)-1:
+    prev=items[y]
+    for i in items[y:]:
+        print(i)
+        if i==prev:
+            #should we del() here?or slice??
+            count +=1
+            count_items[i]=count
+        elif i != prev: #or items.index(i) == len(items)-1:
+            continue
+        #when one i is unique? and (after..) not in items?
+        else:count=1;count_items[i]=count
+    y+=1
+
 
 print(count_items)
