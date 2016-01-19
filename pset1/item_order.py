84d5df0dd7b94ae7534f3ea64fe0eaeabc823b78
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-19 22:11:48 +0100

    done

--- pset1/item_order.py
+++ pset1/item_order.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
 
-#FORMAT: order "salad hamburger water"
-#PRINT: [name][:][count][space]
-s=input("Give words separated by spaces plz: ")
-
-items=list(s.split())
-count_items={'salad':0,'hamburger':0,'water':0}
-
-count=0
-y=0
-while y <= len(items)-1:
-    prev=items[y]
-    for i in items:
-        if i==prev:
-            count +=1
-            count_items[i]=count
+def item_order(order):
+    """
+    This must be written as a function which must return sth
+    here a well-formatted string
+    """
+    items=list(order.split())
+    count_items={'salad':0,'hamburger':0,'water':0}
     count=0
-    y+=1
-
-print(count_items)
+    y=0
+    while y <= len(items)-1:
+        prev=items[y]
+        for i in items:
+            if i==prev:
+                count +=1
+                count_items[i]=count
+        count=0
+        y+=1
+    #string formatting cf 7.1.1 Python Tut
+    result='salad:{salad:d} hamburger:{hamburger:d} water:{water:d}'.format(**count_items)
+    return result
