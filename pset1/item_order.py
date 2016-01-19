de98194b10b6109fee509ab81cfe45709d4d3e0d
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-19 21:39:54 +0100

    modified:   item_order.py

--- pset1/item_order.py
+++ pset1/item_order.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python
-# this is a rewrite exercise of count() method
-# do NOT use count is a goal in itself!!
 
-# TODO: the corner case where i is NOT prev
-# BUT is already a value, should just update the key
-# s= salad hamburger water hamburger
-
+#FORMAT: order "salad hamburger water"
+#PRINT: [name][:][count][space]
 s=input("Give words separated by spaces plz: ")
 
 items=list(s.split())
-count_items={}
+count_items={'salad':0,'hamburger':0,'water':0}
 
 count=0
 y=0
-#pour chaque prev il faut parcourir toute la liste
 while y <= len(items)-1:
     prev=items[y]
-    print(prev)
     for i in items:
-        if i in list(count_items.keys()):
-            # we already got it
-            continue
-        elif i==prev:
-            #should we del() here?or slice??
+        if i==prev:
             count +=1
-            count_items[prev]=count
-        elif items[y] != prev: #or items.index(i) == len(items)-1:
-            continue
-        #when one i is unique? and (after..) not in items?
-        else:count=1;count_items[prev]=count
+            count_items[i]=count
+    count=0
     y+=1
-
 
 print(count_items)
