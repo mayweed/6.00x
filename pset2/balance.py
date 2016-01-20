803adfe8a45b9890a462d63ac0e722939e464a03
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-20 15:44:26 +0100

    Only total paid at the end is missing

--- pset2/balance.py
+++ pset2/balance.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 
-balance=4213
+balance=4842
 annualInterestRate=0.2
 monthlyPaymentRate=0.04
-#To be updated
-prev_balance=0
-#the one to be calculated
-miniMonthlyPayement=0
-
-#The maths
 monthlyInterestRate=annualInterestRate/12.0
-miniMonthlyPayment=monthlyPaymentRate*prev_balance
+paid=[]
+
+def balanceAfterInterest(balance,monthlyInterestRate):
+    unpaid_balance= balance - miniMonthlyPayment
+    interest=unpaid_balance + (monthlyInterestRate*unpaid_balance)
+    return round(interest,2)
+
+for i in range(1,13):
+    miniMonthlyPayment=round(balance*monthlyPaymentRate,2)
+    paid.append(miniMonthlyPayment)
+    print("Month:",i)
+    print("Minimum Monthly Payment:",miniMonthlyPayment)
+    print("Remaining balance:",balanceAfterInterest(balance,monthlyInterestRate))
+    balance=balanceAfterInterest(balance,monthlyInterestRate)
