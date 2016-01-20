452d81c34978e78e112c02554edcc44ddf30978f
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-20 15:54:24 +0100

    formatting

--- pset2/balance.py
+++ pset2/balance.py
@@ -14,7 +14,16 @@ def balanceAfterInterest(balance,monthlyInterestRate):
 for i in range(1,13):
     miniMonthlyPayment=round(balance*monthlyPaymentRate,2)
     paid.append(miniMonthlyPayment)
-    print("Month:",i)
-    print("Minimum Monthly Payment:",miniMonthlyPayment)
-    print("Remaining balance:",balanceAfterInterest(balance,monthlyInterestRate))
+    if i < 12:
+        print("Month:",i)
+        print("Minimum Monthly Payment:",miniMonthlyPayment)
+        print("Remaining balance:",balanceAfterInterest(balance,monthlyInterestRate))
+
+    if i==12:
+        print("Month:",i)
+        print("Minimum Monthly Payment:",miniMonthlyPayment)
+        print("Remaining balance:",balanceAfterInterest(balance,monthlyInterestRate))
+        print("Total paid:",round(sum(paid),2))
+        print("Remaining balance:",balanceAfterInterest(balance,monthlyInterestRate))
+
     balance=balanceAfterInterest(balance,monthlyInterestRate)
