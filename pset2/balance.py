f06f1733d8c9436657cb140c9f576ed2cda7c067
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-20 13:36:58 +0100

    Need to write that down FIRST

--- pset2/balance.py
+++ pset2/balance.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python
+
+balance=4213
+annualInterestRate=0.2
+monthlyPaymentRate=0.04
+#To be updated
+prev_balance=0
+#the one to be calculated
+miniMonthlyPayement=0
+
+#The maths
+monthlyInterestRate=annualInterestRate/12.0
+miniMonthlyPayment=monthlyPaymentRate*prev_balance
