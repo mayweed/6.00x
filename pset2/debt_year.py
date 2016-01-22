0c23757d7984dee1d5ec754eced2a3ae6fc5e903
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-22 16:11:06 +0100

    to save roundupdown before deletion

--- pset2/debt_year.py
+++ pset2/debt_year.py
@@ -1,24 +1,43 @@
 #!/usr/bin/python
 
-balance=3329
-annualInterestRate=0.18
+balance=4773
+annualInterestRate=0.2
+
+owed_sum=balance
+
+def roundupdown(num):
+    #Must be a multiple of ten
+    if num%10 !=0:
+        if num%10 > 5:
+            #round up
+            num += (10 - (num%10))
+        elif num%10 < 5:
+            #round down
+            num-=(num%10)
+    return round(num)
+
+#MATHS
 monthlyInterestRate=annualInterestRate/12.0
+monthlyPayment= roundupdown((balance * (1 + monthlyInterestRate))/12) 
 
-#That's what I must guess(cf upper bound)
-#Should decrease that inside the loop
-monthlyPayment= round((balance * (1 + monthlyInterestRate))/12) 
-print(round(monthlyPayment))
+def remaining_balance(owed_sum):
+    unpaid_balance= owed_sum - monthlyPayment
+    x=unpaid_balance + (monthlyInterestRate*unpaid_balance)
+    return round(x)
 
-def balanceAfterInterest(balance,monthlyInterestRate):
-    unpaid_balance= balance - monthlyPayment
-    interest=unpaid_balance + (monthlyInterestRate*unpaid_balance)
-    return round(interest,2)
-
-#loop condition not good
-while balanceAfterInterest(balance,monthlyInterestRate) > 0:
+def calculate_balance(owed_sum,monthlyPayment):
     for i in range(1,11):
-        print("Month:",i)
-        print("Minimum Monthly Payment:",monthlyPayment)
-        print("Remaining balance:",balanceAfterInterest(balance,monthlyInterestRate))
-        balance=balanceAfterInterest(balance,monthlyInterestRate)
-    monthlyPayment-=10
+        print("balance ",owed_sum)
+        print("MonthlyPayment ",monthlyPayment)
+        x= remaining_balance(owed_sum)
+        print("Month/remB ",i,x)
+        owed_sum=x
+    return owed_sum
+
+x=calculate_balance(owed_sum,monthlyPayment)
+while x > 0:
+    owed_sum=balance
+    monthlyPayment += 10
+    x=calculate_balance(owed_sum,monthlyPayment)
+
+print(monthlyPayment)
