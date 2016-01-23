9a31916acbe6d32ee67bb5624502b9272ec98511
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-23 08:36:37 +0100

    modified:   pset2/debt_year.py

--- pset2/debt_year.py
+++ pset2/debt_year.py
@@ -8,6 +8,7 @@ monthlyInterestRate=annualInterestRate/12.0
 #Should decrease that inside the loop
 monthlyPayment= round((balance * (1 + monthlyInterestRate))/12) 
 print(round(monthlyPayment))
+remaining_balance=balance
 
 def balanceAfterInterest(balance,monthlyInterestRate):
     unpaid_balance= balance - monthlyPayment
@@ -15,10 +16,14 @@ def balanceAfterInterest(balance,monthlyInterestRate):
     return round(interest,2)
 
 #loop condition not good
-while balanceAfterInterest(balance,monthlyInterestRate) > 0:
+while balanceAfterInterest(remaining_balance,monthlyInterestRate) > 0:
     for i in range(1,11):
+        month=i
         print("Month:",i)
         print("Minimum Monthly Payment:",monthlyPayment)
         print("Remaining balance:",balanceAfterInterest(balance,monthlyInterestRate))
-        balance=balanceAfterInterest(balance,monthlyInterestRate)
+        remaining_balance=balanceAfterInterest(balance,monthlyInterestRate)
+        if remaining_balance <=0 :break
     monthlyPayment-=10
+    #go for another one with diff payment but same original balance
+remaining_balance=balance
