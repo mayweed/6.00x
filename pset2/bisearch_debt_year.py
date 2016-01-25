2b132231c10eb2c5769b5f1b08ee19d9a1e08016
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-25 16:02:54 +0100

    Mmm will be difficult...

--- pset2/bisearch_debt_year.py
+++ pset2/bisearch_debt_year.py
@@ -2,38 +2,47 @@
 
 """
 Those 2 values for testing purposes only
+Res=90325.03
 """
 balance=999999
 annualInterestRate=0.18
 
 owed_sum=balance
 monthlyInterestRate=annualInterestRate/12.0
-monthlyPayment=10.0 
 
 lower_bound=round((balance/12.0),2)
-print(lower_bound)
 upper_bound=round(balance *((1 + monthlyInterestRate)**12)/12.0,2)
-print(upper_bound)
-mid=lower_bound+(upper_bound-lower_bound)/2
-print(mid)
+mid=lower_bound+(upper_bound-lower_bound)/2.0
+
+#compare it to remaining balance at the end of the year
+# si l'ecart entre remainingbalance et 0 est > à epsilon
+epsilon=0.1
 
 def remaining_balance(owed_sum):
     unpaid_balance= owed_sum - monthlyPayment
     x=unpaid_balance + (monthlyInterestRate*unpaid_balance)
-    return round(x,2)
+    return x
 
 def calculate_balance(owed_sum,monthlyPayment):
     for i in range(1,13):
-        #print("MonthlyPayment ",monthlyPayment)
+        print("MonthlyPayment ",monthlyPayment)
         x= remaining_balance(owed_sum)
-        #print("Month/remB ",i,x)
+        print("Month/remB ",i,x)
         owed_sum=x
     return owed_sum
 
+monthlyPayment=mid
 x=calculate_balance(owed_sum,monthlyPayment)
-while x >= 0:
+while x - 0 > epsilon:
+#    owed_sum=balance
+#    x=calculate_balance(owed_sum,monthlyPayment)
+if  < epsilon:
     owed_sum=balance
-    monthlyPayment += 10
+    upper_bound=mid
+    mid=lower_bound+(upper_bound-lower_bound)/2.0
+    monthlyPayment=mid
     x=calculate_balance(owed_sum,monthlyPayment)
+    if x > epsilon:
+
 
 print("Lowest payment: ",monthlyPayment)
