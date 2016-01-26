1d2c9210ec27dfc16ebe98e00850b9e87996a2da
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-26 17:06:11 +0100

    91485 vs 90325 am not that far away from LA...

--- pset2/bisearch_debt_year.py
+++ pset2/bisearch_debt_year.py
@@ -18,36 +18,50 @@ mid=lower_bound+(upper_bound-lower_bound)/2.0
 # si l'ecart entre remainingbalance et 0 est > à epsilon
 epsilon=0.1
 
-def remaining_balance(owed_sum):
+def remaining_balance(owed_sum,monthlyPayment):
     unpaid_balance= owed_sum - monthlyPayment
     x=unpaid_balance + (monthlyInterestRate*unpaid_balance)
     return x
 
-def calculate_balance(owed_sum,monthlyPayment):
-    for i in range(1,13):
-        print("MonthlyPayment ",monthlyPayment)
-        x= remaining_balance(owed_sum)
-        print("Month/remB ",i,x)
-        owed_sum=x
-    return owed_sum
-
 monthlyPayment=mid
-x=calculate_balance(owed_sum,monthlyPayment)
-#while x - 0 > epsilon:
-#    owed_sum=balance
-#    x=calculate_balance(owed_sum,monthlyPayment)
-if x < epsilon:
-    owed_sum=balance
-    upper_bound=mid
-    mid=lower_bound+(upper_bound-lower_bound)/2.0
+#x=calculate_balance(owed_sum,monthlyPayment)
+#You begin with a neat balance
+remainingBalance=balance
+while remainingBalance > 0:
+    remainingBalance=balance
     monthlyPayment=mid
-    x=calculate_balance(owed_sum,monthlyPayment)
-    """
-    if x > epsilon:
-        owed_sum=balance
-        low=mid
-        mid=lower_bound+(upper_bound-lower_bound)/2.0
-        monthlyPayment=mid
-        x=calculate_balance(owed_sum,monthlyPayment)
-"""
+    for i in range(1,13):
+        owed=remaining_balance(remainingBalance,monthlyPayment)
+        print(i,owed,monthlyPayment)
+        #should update that value between each iteration
+        #unless infinite loop...
+        remainingBalance=owed
+        if owed - epsilon <0: 
+            upper_bound=mid
+            mid=lower_bound+(upper_bound-lower_bound)/2.0
+            monthlyPayment=mid
+            remainingBalance=balance
+            for i in range(1,13):
+                owed=remaining_balance(remainingBalance,monthlyPayment)
+                print(i,owed,monthlyPayment)
+                #should update that value between each iteration
+                #unless infinite loop...
+                remainingBalance=owed
+        if owed-epsilon > epsilon:
+            lower_bound=mid
+            mid=lower_bound+(upper_bound-lower_bound)/2.0
+            monthlyPayment=mid
+            remainingBalance=balance
+            for i in range(1,13):
+                owed=remaining_balance(remainingBalance,monthlyPayment)
+                print(i,owed,monthlyPayment)
+                #should update that value between each iteration
+                #unless infinite loop...
+                remainingBalance=owed
+
+
+    #if what you owed is > to 0 loop again oki?
+    remainingBalance=owed
+    print(owed)
+
 print("Lowest payment: ",monthlyPayment)
