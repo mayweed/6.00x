75931cfed7f1ab2dea598bca7ad6c35f70128e8d
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-26 16:24:23 +0100

    No good time to think

--- pset2/bisearch_debt_year.py
+++ pset2/bisearch_debt_year.py
@@ -33,17 +33,21 @@ def calculate_balance(owed_sum,monthlyPayment):
 
 monthlyPayment=mid
 x=calculate_balance(owed_sum,monthlyPayment)
-while x - 0 > epsilon:
+#while x - 0 > epsilon:
 #    owed_sum=balance
 #    x=calculate_balance(owed_sum,monthlyPayment)
-if  < epsilon:
+if x < epsilon:
     owed_sum=balance
     upper_bound=mid
     mid=lower_bound+(upper_bound-lower_bound)/2.0
     monthlyPayment=mid
     x=calculate_balance(owed_sum,monthlyPayment)
+    """
     if x > epsilon:
-    # low=mid?
-
-
+        owed_sum=balance
+        low=mid
+        mid=lower_bound+(upper_bound-lower_bound)/2.0
+        monthlyPayment=mid
+        x=calculate_balance(owed_sum,monthlyPayment)
+"""
 print("Lowest payment: ",monthlyPayment)
