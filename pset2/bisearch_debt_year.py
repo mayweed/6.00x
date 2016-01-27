efbb0225f7529d1961b4b548e1768b5f90967fc0
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-27 16:23:11 +0100

    Thx to the inconsistencies in the output i found the solution: should only move payment like ans in the lecture...

--- pset2/bisearch_debt_year.py
+++ pset2/bisearch_debt_year.py
@@ -14,7 +14,6 @@ lower_bound=round((balance/12.0),2)
 upper_bound=round(balance *((1 + monthlyInterestRate)**12)/12.0,2)
 mid=lower_bound+(upper_bound-lower_bound)/2.0
 monthlyPayment=mid
-epsilon=0.2
 
 def remaining_balance(owed_sum):
     """
@@ -30,27 +29,20 @@ def calculate_balance(owed_sum,monthlyPayment):
         Yields the remaining balance after 12 months of payment
     """
     for i in range(1,13):
-        #print("MonthlyPayment ",monthlyPayment)
         x= remaining_balance(owed_sum)
-        #print("Month/remB ",i,x)
         owed_sum=x
     return owed_sum
 
 remainingBalance=balance
-while abs(remainingBalance) >= 0 :
+while abs(remainingBalance) >= epsilon :
     owed_sum=balance
+    monthlyPayment=lower_bound+(upper_bound-lower_bound)/2.0
     remainingBalance=calculate_balance(owed_sum,monthlyPayment)
-    print("REMB:"remainingBalance)
 
     if remainingBalance < 0: 
-        high=monthlyPayment
-        low=lower_bound
+        upper_bound=monthlyPayment
         
     if remainingBalance > 0: 
-        high=upper_bound
-        low=monthlyPayment
+        lower_bound=monthlyPayment
 
-    monthlyPayment=low+(high-low)/2.0
-    print("L/M/H:",low,monthlyPayment,high)
-
-#print("Lowest payment: ",round(monthlyPayment,2))
+print("Lowest payment: ",round(monthlyPayment,2))
