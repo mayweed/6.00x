2bd0d71ff88c3db471231065c2e2434e02e3a899
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-28 13:12:25 +0100

    add comments

--- pset1/polysum.py
+++ pset1/polysum.py
@@ -3,10 +3,16 @@
 import math
 
 def polysum(n,s):
+    """
+        This function takes as input the number of sides n and the length
+        of each side s of a polygon. It returns as output the sum of the
+        area and the square of the polygon perimeter .
+    """
     def perimeter(numberofside,length):
         return numberofside*length
 
     def area(numberofside,length):
         aire=((0.25*n*s**2)/math.tan(math.pi/n))
         return round(aire,2)
+
     return round(area(n,s)+perimeter(n,s)**2,4)
