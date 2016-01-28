2ec732cf0aa40cedc16555f1fe859c14823a3dac
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-28 14:09:26 +0100

    Add comment

---
+++ pset2/ndigits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+#that one should make a recursive call
+n=10000
+
+def ndigits(number):
+    """
+        This function takes as input a number postive or negative and outputs the number of
+        digits in number.
+    """
+    count=1
+    if abs(number//10) == 0:return 1
+    else:return count+1*ndigits(abs(number//10))
