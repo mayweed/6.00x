417b35e2ea1814acd54d464f1283b0586b85c562
Guillaume Raimondeau <raimondeau@gmail.com>
2016-01-29 15:46:17 +0100

    Does not work with float!! And it's not as recursive as I wish it to be!!

--- pset3/radexposure.py
+++ pset3/radexposure.py
@@ -2,27 +2,44 @@
 
 import math
 
+#!! does not work with float step !!
 #Test case 1
 start=0
-stop=5
-step=1
+stop=4
+step=0.25
+
 
 def f(x):
     return 10*math.e**(math.log(0.5)/5.27 * x)
 
-def radiationExposure(start, stop, step):
-'''
-Computes and returns the amount of radiation exposed
-to between the start and stop times. Calls the 
-function f (defined for you in the grading script)
-to obtain the value of the function at any point.
-                 
-start: integer, the time at which exposure begins
-stop: integer, the time at which exposure ends
-step: float, the width of each rectangle.
-You can assume that the step size will always partition the space evenly.
+def rectangle_area(start,step):
+    '''
+        compute the area of a rectangle with length as start point
+        and step as width
+    '''
+    length=f(start)
+    return length*step
 
-returns: float, the amount of radiation exposed to between start and stop times.
-'''
-#the amount of radiation
-rad=0
+def radiationExposure(start, stop, step):
+    '''
+    Computes and returns the amount of radiation exposed
+    to between the start and stop times. Calls the 
+    function f (defined for you in the grading script)
+    to obtain the value of the function at any point.
+                 
+    start: integer, the time at which exposure begins
+    stop: integer, the time at which exposure ends
+    step: float, the width of each rectangle.
+    You can assume that the step size will always partition the space evenly.
+
+    returns: float, the amount of radiation exposed to between start and stop times.
+    '''
+    rad=0.0
+    #the amount of radiation
+    while start < stop:
+        rad+=rectangle_area(start,step) 
+        start+=step
+        print(start,rad)
+    return rad
+
+radiationExposure(start, stop, step)
