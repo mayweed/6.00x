7982f3ce6461180010c0aa699a64401bb85385b1
Guillaume Raimondeau <raimondeau@gmail.com>
2016-02-20 17:15:55 +0100

    Some tweaks

--- pset6/mk_dict.py
+++ pset6/mk_dict.py
@@ -4,10 +4,19 @@ import string
 
 #am lazy cant write the 26 letters etc by hand
 #could reuse that: in a func + python -c?? Argv??
-count=1
-print('{',end='')
-for i in string.ascii_lowercase: 
-    print("\'{0}\':{1},".format(i,str(count)),end='')
-    count+=1
-    if count==14: print("\n",end='')
-print('}')
+def mk_dict(L):
+    '''
+        Take an iterable
+        Return a dict numbered from 0
+    '''
+    count=1
+    print('{',end='')
+    for i in L:
+        print("\'{0}\':{1},".format(i,str(count)),end='')
+        count+=1
+        if count==14: print("\n",end='')
+    print('}')
+
+### TESTING ###
+#mk_dict(string.ascii_lowercase)
+#mk_dict(['a','b','c'])
