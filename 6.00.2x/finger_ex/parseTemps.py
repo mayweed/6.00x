88ce20dfa80226797c12cd7140f2b814eda5897b
Guillaume Raimondeau <raimondeau@gmail.com>
2016-03-21 10:52:03 +0100

    Down with that parsing, very nice exercise btw

--- 6.00.2x/finger_ex/parseTemps.py
+++ 6.00.2x/finger_ex/parseTemps.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+fileTemps='julyTemps.txt'
+
 def parseTemps(fileTemps):
-    with open(fileTemps) as inFile:
+    highTemps=[]
+    lowTemps=[]
+    fields=[]
+    with open(fileTemps, 'r') as inFile:
+        for line in inFile:
+            #want to get read of that first
+            if len(line) < 3 or not line[0].isdigit(): continue
+            else:fields.append(line.split())
+
+        for field in fields:
+            highTemps.append(int(field[1]))
+            lowTemps.append(int(field[2]))
         
+        count=0
+        while count < len(fields):
+            print((highTemps[count],lowTemps[count]))
+            count+=1
+            
+parseTemps(fileTemps)        
