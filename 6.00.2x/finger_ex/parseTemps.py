639c68bb15ceb119aa81cc41af91090f869b2e4f
Guillaume Raimondeau <raimondeau@gmail.com>
2016-03-21 11:00:03 +0100

    with fields as str you avoid the double loop and keep the indexing

--- 6.00.2x/finger_ex/parseTemps.py
+++ 6.00.2x/finger_ex/parseTemps.py
@@ -5,20 +5,18 @@ fileTemps='julyTemps.txt'
 def parseTemps(fileTemps):
     highTemps=[]
     lowTemps=[]
-    fields=[]
     with open(fileTemps, 'r') as inFile:
         for line in inFile:
             #want to get read of that first
-            if len(line) < 3 or not line[0].isdigit(): continue
-            else:fields.append(line.split())
+            fields=line.split()
 
-        for field in fields:
-            highTemps.append(int(field[1]))
-            lowTemps.append(int(field[2]))
+            if len(line) < 3 or not line[0].isdigit(): 
+                continue
+            else: 
+                highTemps.append(int(fields[1]))
+                lowTemps.append(int(fields[2]))
         
-        count=0
-        while count < len(fields):
-            print((highTemps[count],lowTemps[count]))
-            count+=1
+        # A tuple of the two lists!!
+        return(highTemps,lowTemps)
             
 parseTemps(fileTemps)        
